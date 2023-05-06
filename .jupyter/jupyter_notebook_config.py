83b4c486c467ab81c9f3d84e0bfbf7929c94a510
Kerrick Staley <k@kerrickstaley.com>
2023-05-06 17:06:36 -0400

    Add hook to strip trailing whitespace when saving a Jupyter notebook

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1438,3 +1438,33 @@ c = get_config()  #noqa
 #  inactive timeout value.
 #  Default: 300
 # c.TerminalManager.cull_interval = 300
+
+
+# Strip trailing whitespace on save
+# https://github.com/jupyter/notebook/issues/1455
+#------------------------------------------------------------------------------
+# Custom whitespace stripping
+#------------------------------------------------------------------------------
+def strip_white_space(text):
+    return '\n'.join([line.rstrip() for line in text.split('\n')])
+
+def scrub_output_pre_save(model=None, **kwargs):
+    """Auto strip trailing white space before saving."""
+    # If we are dealing with a notebook #
+    if model['type'] == 'notebook':
+        # Don't run on anything other than nbformat version 4 #
+        if model['content']['nbformat'] != 4:
+            print("Skipping white space stripping since `nbformat` != 4.")
+            return
+        # Apply function to every cell #
+        print("Stripping white space on a notebook.")
+        for cell in model['content']['cells']:
+            if cell['cell_type'] != 'code': continue
+            cell['source'] = strip_white_space(cell['source'])
+    # If we are dealing with a file #
+    if model['type'] == 'file':
+        if model['format'] == 'text':
+            print("Stripping white space on a file.")
+            model['content'] = strip_white_space(model['content'])
+
+c.ContentsManager.pre_save_hook = scrub_output_pre_save
